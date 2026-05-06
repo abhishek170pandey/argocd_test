fea105a2f85871e4a59e65e91613b678faaf7b6e
abhishek170pandey <166979882+abhishek170pandey@users.noreply.github.com>
2026-05-06 13:20:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! from Abhi !!!!! Welcome to GitOps World !!!!"
+    return "Hello World! from Abhi !!!! Welcome to GitOps World !!!!"
